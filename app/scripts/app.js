7007f231e7979c174ee3d919621019503c6c8aab
Yaron Miro <yaron@gizra.com>
2014-12-24 09:59:38 +0200

    WIP: Add app.

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myAppApp
+ * @description
+ * # myAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myAppApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myAppApp
+ */
+angular.module('myAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
