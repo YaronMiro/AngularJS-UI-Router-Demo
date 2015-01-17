4431b32e8e0b5add39f6bf4627f0f129cd69361b
yaronmiro <yaron@gizra.com>
2015-01-17 21:35:54 +0200

    Clean code.

--- app/scripts/services/movies.js
+++ app/scripts/services/movies.js
@@ -31,32 +31,6 @@ angular.module('myApp')
     }
 
     /**
-     * Return the request movie trailer (Embed code) by movie name.
-     *
-     * @param name
-     *  string | Movie name.
-     *
-     * @returns string (html markup)
-     */
-    function requestMovieTrailer(name) {
-
-      var deferred = $q.defer();
-      $http({
-        method: 'GET',
-        url: 'http://www.myapifilms.com/taapi?film=' + name + '&width=000&count=1&format=JSON'
-      })
-        .success(function(data) {
-          console.log(data);
-        })
-        .error(function(data) {
-          console.log('Error: ', data);
-        })
-
-      // Return promise object.
-      return deferred.promise;
-    }
-
-    /**
      * Return the promise {*} with the list of top movies data.
      *
      * @param ids
@@ -76,9 +50,7 @@ angular.module('myApp')
           // Ad an extra movie image sizes 300px | 600px width.
           angular.forEach(movies.results, function(movie) {
             var artworkUrl600 = movie.artworkUrl100.replace('100x100', "600x600");
-            var artworkUrl200 = movie.artworkUrl100.replace('100x100', "200x200");
             movie.artworkUrl600 = artworkUrl600;
-            movie.artworkUrl200 = artworkUrl200;
           });
 
           deferred.resolve(movies.results);
