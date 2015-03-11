556a73ea5351dbacdfc0e9b90839d89a15bd3061
yaronmiro <yaron@gizra.com>
2015-03-11 20:59:49 +0200

    Adding diective

--- app/scripts/controllers/movie.js
+++ app/scripts/controllers/movie.js
@@ -8,10 +8,11 @@
  * Controller of the myApp
  */
 angular.module('myApp')
-  .controller('MovieCtrl', ['$scope', '$stateParams', '$state', 'Movies', 'selectedMovie', function ($scope, $stateParams, $state, Movies, selectedMovie) {
+  .controller('MovieCtrl', ['$scope', '$stateParams', '$state', 'Movies', 'selectedMovie', 'movies', function ($scope, $stateParams, $state, Movies, selectedMovie, movies) {
 
     // Selected movie.
     $scope.selectedMovie = selectedMovie;
+    $scope.movies = movies;
 
     // Verify that we have the data object declared.
     if (angular.isDefined($state.current.data)) {
