4c621fa82afa7c7ae27c64b1dd949bfd881f5dc0
yaronmiro <yaron@gizra.com>
2015-04-11 19:05:41 +0300

    Adding movies to bookmark

--- app/scripts/services/bookmarks.js
+++ app/scripts/services/bookmarks.js
@@ -3,9 +3,6 @@
 angular.module('myApp')
   .factory('Bookmarks', ['localStorageService', function (localStorageService) {
 
-    // A private cache key.
-    var movies = [];
-
     return {
 
       /**
@@ -18,21 +15,25 @@ angular.module('myApp')
        * @returns bool
        */
       addToBookmarks: function(movie) {
-      movies.push(movie);
-      return localStorageService.set('bookmarks', movies);
+        // Get array of movies.
+        var movies = localStorageService.get('bookmarks') != null ? localStorageService.get('bookmarks') : new Array();
+        // Set a new index for the incoming movie.
+        movie.index = movies.length ? (movies.length + 1) : 1;
+        // Add movie to the array.
+        movies.push(movie);
+        // Update the local storage value.
+        return localStorageService.set('bookmarks', movies);
     },
 
     /**
      * Get all of the movies from the local storage "movies" array.
      * On success return the value from the local storage.
      *
-     *
      * @returns bool
      */
     getMovies: function() {
       return localStorageService.get('bookmarks');
     }
-
    }
 
   }]);
