8f36185c3b12ef830dad24fe55014e6f9db23170
yaronmiro <yaron@gizra.com>
2015-06-16 05:35:18 +0300

    Trying to make somthing

--- app/scripts/directives/scrollToMovie.js
+++ app/scripts/directives/scrollToMovie.js
@@ -11,13 +11,23 @@ angular.module('myApp')
         element.bind('click', function(){
 
           // Make the "summary" view visible.
-          console.log(scope.movie);
-          scope.movie.viewMode = !scope.movie.viewMode;
+//          console.log(scope.movie);
+//          scope.movie.viewMode = !scope.movie.viewMode;
 
           // Get the movie wrapper.
           var movieElementId = '#' + scope.movie.id;
           var elementMovieWrapper = angular.element(movieElementId);
 
+          var links = elementMovieWrapper.find('.links').children();
+
+
+
+          angular.element(links[0]).bind('click', function(){
+            var siblings = angular.element('.movies-wrapper');
+            angular.forEach(siblings, function(element){
+              angular.element(element).css('opacity', 1.0);
+            });
+          });
 
           // Restore opacity.
           elementMovieWrapper.css('opacity', 1.0);
@@ -25,7 +35,7 @@ angular.module('myApp')
           // Target all of the "movie-wrapper" siblings and set their opacity.
           var siblings = angular.element('.movies-wrapper:not(' + movieElementId +')');
           angular.forEach(siblings, function(element){
-            angular.element(element).css('opacity', 0.2);
+            angular.element(element).css('opacity', 0.05);
           });
 
          // scroll to target movie.
